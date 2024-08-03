13fe741ff975d00aafb252a896956dc2a400c1bc
Bellorico323 <mabo.orico@gmail.com>
2024-08-03 12:28:21 -0300

    feat: invites feature

---
+++ apps/web/src/http/get-invites.ts
@@ -0,0 +1,26 @@
+import { api } from './api-client'
+
+interface GetInvitesResponse {
+  invites: {
+    id: string
+    email: string
+    role: 'ADMIN' | 'MEMBER' | 'BILLING'
+    createdAt: string
+    author: {
+      id: string
+      name: string | null
+    } | null
+  }[]
+}
+
+export async function getInvites(org: string) {
+  const result = await api
+    .get(`organizations/${org}/invites`, {
+      next: {
+        tags: [`${org}/invites`],
+      },
+    })
+    .json<GetInvitesResponse>()
+
+  return result
+}
